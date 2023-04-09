75eabbe414c85e49cf4eaf9d57a3d6fdd81bb065
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-04-09 17:30:49 +0530

    setting

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -16,8 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'cafusabcdefghiacfdeysaczdsfoapqwsbbbbbbbbbbbsaaaaaaaaaaa' #this is the dummy key
