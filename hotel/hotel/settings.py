5a2f4c1d04ed62c7ba19d8a46fa41253ccd8680f
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-04-09 17:27:37 +0530

    Try to resolve Some fault

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Forget Password 
 EMAIL_BACKED = 'django.core.mail.backends.smtp.EmialBacked'
